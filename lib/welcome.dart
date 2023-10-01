16b347e1e47dbe0b2a3e48bbbadfe3067c5bb505
Anchalpreet1082 <146555072+Anchalpreet1082@users.noreply.github.com>
2023-10-01 19:06:50 +0530

    Updated welcome.dart

---
+++ lib/welcome.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:try_project/dashboard.dart';
+
+class Welcome extends StatelessWidget {
+  const Welcome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:
+      Container(
+
+        width: double.infinity,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+          // gradient: LinearGradient(
+          //     colors: [
+          //       Colors.white10,
+          //       Colors.white24,
+          //     ],
+          //     begin: Alignment.topLeft,
+          //     end: Alignment.bottomRight,
+          //     stops: [0.0, 1.0],
+          //     tileMode: TileMode.clamp)
+          image: DecorationImage(
+            image: AssetImage("assets/university.webp"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Stack(
+            children: [
+              Positioned(
+                bottom: 650,
+                left: 0,
+                right: 0,
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'assets/logo_au1.png',
+                      width: 380,
+                    ),
+                    // const Text(
+                    //   'Welcome',
+                    //   style: TextStyle(
+                    //     fontSize: 29,
+                    //     color: Colors.amber,
+                    //   ),
+                    // )
+                  ],
+                ),
+              ),
+              Positioned(
+                  bottom: 100,
+                  left: 0,
+                  right: 0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(40.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.pushReplacement(context,
+                            MaterialPageRoute(builder: (_) =>  Dashboard()));
+                      },
+                      child: Container(
+                        height: 50,
+                        decoration: const BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.all(Radius.circular(30)),
+                        ),
+                        child: const Center(
+                          child: Text(
+                            'Get Started',
+                            style: TextStyle(
+                              color: Colors.amber,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
